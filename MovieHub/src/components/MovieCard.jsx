b7c5e94a3c20d49a64e7b78638cac602f10e7b29
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-08-01 08:47:50 +0530

    a

--- MovieHub/src/components/MovieCard.jsx
+++ MovieHub/src/components/MovieCard.jsx
@@ -1,13 +1,25 @@
 function MovieCard({movie}) {
 
+   function onFavoriteClick(){
+    alter("clicked")
+   }
+
     return (
         <div className="movie-card">
         <div className="movie-poster">
         <img src={movie.url}  alt ={movie.title} />
-        <div className="movie-overlay"> <button classname="favorite-btn" onClick={onFavoriteClick}></button> 
+        <div className="movie-overlay"> <button classname="favorite-btn" onClick={onFavoriteClick}>
+            ♥
+            </button> 
             
         </div>
+        </div>
+        <div className="movie-info">
+            <h3>movie.title</h3>
+            <p>{movie.release_date}</p>
         </div>
         </div>
     );
 }
+
+export default MovieCard
