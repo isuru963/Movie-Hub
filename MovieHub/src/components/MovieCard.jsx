cf292f0705536f95f479fa0ee7344b034e3993d7
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-08-01 08:40:04 +0530

    a

--- MovieHub/src/components/MovieCard.jsx
+++ MovieHub/src/components/MovieCard.jsx
@@ -3,8 +3,10 @@ function MovieCard({movie}) {
     return (
         <div className="movie-card">
         <div className="movie-poster">
-        <img />
-
+        <img src={movie.url}  alt ={movie.title} />
+        <div className="movie-overlay"> <button classname="favorite-btn" onClick={onFavoriteClick}></button> 
+            
+        </div>
         </div>
         </div>
     );
