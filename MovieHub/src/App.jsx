956a3aaccf4147006f36c799a1ff2153f284cd66
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-08-01 08:22:12 +0530

    a

--- MovieHub/src/App.jsx
+++ MovieHub/src/App.jsx
@@ -2,21 +2,14 @@ import "./App.css";
 
 function App(){
   return(
-    <>
-      <Text display="Hello" />
-      <Text display="World!" />
-    </>
-  )
+   <>
+   
+   </>
 
-}
-
-function Text({display}) {
-  return(
-    <div>
-      <p>{display}</p>
-    </div>
   );
+
 }
+
 
 export default App;
 
