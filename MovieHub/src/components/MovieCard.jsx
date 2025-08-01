ac646828016af43e16c74c6b643207ecb1425690
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-08-01 08:27:24 +0530

    a

---
+++ MovieHub/src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+function MovieCard({movie}) {
+
+    return (
+        <div className="movie-card">
+        <div className="movie-poster">
+        <img />
+
+        </div>
+        </div>
+    );
+}
